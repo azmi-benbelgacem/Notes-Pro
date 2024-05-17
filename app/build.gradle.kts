be0ba2ccdde25f4eaf2327a65327a344fe60d7c1
benbelgacem azmi <benbelgacemazmi@gmail.com>
2024-05-17 12:08:02 +0100

    last commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,10 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.firebase:firebase-auth:22.3.1")
-    implementation ("com.google.firebase:firebase-firestore:24.11.1")
+    implementation ("com.google.firebase:firebase-auth:23.0.0")
+    implementation ("com.google.firebase:firebase-firestore:25.0.0")
     implementation ("com.firebaseui:firebase-ui-firestore:8.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
